e29ed99306aeacb02abaa3ee79b03c0930481f6c
Christopher Langton <chris@langton.cloud>
2021-07-19 03:09:44 +1000

    add totp and active mfa

--- trivialsec/models/member_mfa.py
+++ trivialsec/models/member_mfa.py
@@ -5,10 +5,13 @@ __module__ = 'trivialsec.models.member_mfa'
 
 class MemberMfa(DatabaseHelpers):
     def __init__(self, **kwargs):
-        super().__init__('member_mfa', 'member_id')
+        super().__init__('member_mfa', 'mfa_id')
+        self.mfa_id = kwargs.get('mfa_id')
         self.member_id = kwargs.get('member_id')
         self.type = kwargs.get('type')
         self.name = kwargs.get('name')
+        self.active = bool(kwargs.get('active'))
+        self.totp_code = kwargs.get('totp_code')
         self.webauthn_id = kwargs.get('webauthn_id')
         self.webauthn_public_key = kwargs.get('webauthn_public_key')
         self.webauthn_challenge = kwargs.get('webauthn_challenge')
