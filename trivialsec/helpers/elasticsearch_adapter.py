7f2f6ee3a35083203217f359e0bf4179a42e572f
chris <chris@trivialsec.com>
2021-08-28 23:39:15 +1000

    dont store private doc

--- trivialsec/helpers/elasticsearch_adapter.py
+++ trivialsec/helpers/elasticsearch_adapter.py
@@ -102,6 +102,7 @@ class Elasticsearch_Document_Adapter:
     def persist(self, extra :dict = None) -> bool:
         doc = vars(self)
         del doc['_Elasticsearch_Document_Adapter__cols']
+        del doc['_doc']
         if isinstance(extra, dict):
             doc = {**doc, **extra}
         res = self.es.index(index=self.__index, id=doc[self.__pk], body=doc)
